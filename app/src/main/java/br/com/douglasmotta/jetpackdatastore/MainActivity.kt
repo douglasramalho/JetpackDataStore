ca6d0fc05e5b9c48b5d874caeda2fb7d92e75cfe
Douglas Motta <tibeca@gmail.com>
2020-11-27 23:08:29 +0000

    Adds ViewBinding to skeleton

--- app/src/main/java/br/com/douglasmotta/jetpackdatastore/MainActivity.kt
+++ app/src/main/java/br/com/douglasmotta/jetpackdatastore/MainActivity.kt
@@ -2,10 +2,16 @@ package br.com.douglasmotta.jetpackdatastore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.douglasmotta.jetpackdatastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
     }
 }
